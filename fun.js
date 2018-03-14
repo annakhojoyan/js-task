21abe09084757dba059ed6aabd7036f62fa3f1ae
annakhojoyan <xojoyana5@gmail.com>
2018-03-14 17:34:31 +0400

    yes shat gidem inch

--- fun.js
+++ fun.js
@@ -1,9 +1,27 @@
-$(document).ready(function () {
+(function () {
+  function init() {
+    $( "#form2" ).fadeOut();
+    let result = '';
+    $( "#form1" ).submit(function( event ) {
+      $( "#form1" ).fadeOut();
+      $( "#form2" ).fadeIn();
+     result = $( "#text" ).val();     
+    });
+    console.log("ahjdhghagdhghjd")
+    $( "#form2" ).submit(function( event ) {
+      $( "#form2" ).fadeOut();
+     result +=  $( "#name" ).val(); 
+     $( "span" ).text( result ).fadeIn();    
+    });
+  }
   function greeting(greeting) {
     return function (name) {
       console.log(greeting + ' ' + name);
     }
   }
-});
-let currentusername = greeting(getElementsByName("text").value);
-currentusername(document.getElementsByName("name").value);
+
+  let currentusername = greeting(text);
+  currentusername(name);
+  init();
+})();
+
