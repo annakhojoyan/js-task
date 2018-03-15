ffb5d319be0a07f9630c62d17ac9f13e903ce134
annakhojoyan <xojoyana5@gmail.com>
2018-03-16 00:42:55 +0400

    fixed errors

--- fun.js
+++ fun.js
@@ -1,25 +1,48 @@
-(function () {
-  function init() {
-    $( "#form2" ).fadeOut();
-    let result = '';
-    $( "#form1" ).submit(function( event ) {
-      $( "#form1" ).fadeOut();
-      $( "#form2" ).fadeIn();
-     result = $( "#text" ).val();     
-    });
-    $( "#form2" ).submit(function( event ) {
-      $( "#form2" ).fadeOut();
-     result +=  $( "#name" ).val(); 
-     $( "#span" ).text( result );    
-    });
+// (function () {
+//   function init() {
+//     $( "#surname" ).fadeOut();
+//     let result = '';
+//     $('#name').keypress(function(e) {
+//       if(e.which == 13) {
+//           console.log($( "#name" ).val());return;
+//       }
+//   });
+//     $( "#form1" ).on('submit', function( event ) {
+//       console.log("sgfjgjhf"); 
+//       return;
+//       $( "#form1" ).fadeOut();
+//       $( "#form2" ).fadeIn();
+//      result = $( "#text" ).val();     
+//     });
+//     $( "#form2" ).on('submit', function( event ) {
+//       $( "#form2" ).fadeOut();
+//      $( "#name" ).val(result); 
+//      console.log(result);
+//      $( "#span" ).text( result );    
+//     });
+//   }
+//   function greeting(greeting) {
+//     return function (name) {
+//       return (greeting + ' ' + name);
+//     }
+//   }
+//   //let currentusername = greeting(text);
+//   //currentusername(name);
+//   //init();
+// })();
+$("#surname").fadeOut();
+var result = '';
+$('#name').keypress(function (e) {
+  if (e.which == 13) {
+    $("#surname").fadeIn();
+    $("#name").fadeOut();
+    result = $("#name").val();
   }
-  function greeting(greeting) {
-    return function (name) {
-      return (greeting + ' ' + name);
-    }
+});
+$('#surname').keypress(function (e) {
+  if (e.which == 13) {
+    $("#surname").fadeOut();
+    result += " " + $("#surname").val();
+    $("#span").text(result);
   }
-
-  let currentusername = greeting(text);
-  currentusername(name);
-  init();
-})();
+});
